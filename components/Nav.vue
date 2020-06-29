84f5b4074b2eaf72587b0c2534f1f42339b040ed
Dagoberto Meza <lashmr@gmail.com>
2020-06-29 12:06:17 -0600

    update navbar.

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="tm-header">
-        <nav class="uk-navbar-container" uk-navbar>
-            <div class="uk-navbar-left">Logo</div>
-        </nav>
+        <div class="uk-container uk-container-expand">
+            <nav class="uk-navbar-container" uk-navbar>
+                <div class="uk-navbar-left">Esto es CHEPE</div>
+                <div class="uk-navbar-right">
+                    <ul class="uk-navbar-nav">
+                        <li><a href="/">Inicio</a></li>
+                        <li><a href="/manifiesto">Manifiesto</a></li>
+                        <li><a href="/galeria">Galeria</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </div>
 </template>
