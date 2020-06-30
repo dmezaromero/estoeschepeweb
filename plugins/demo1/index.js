9ddb4345b7f77e63bd8d59bf49a701d8fcddbc23
Dagoberto Meza <lashmr@gmail.com>
2020-06-29 22:27:34 -0600

    update loading.

--- plugins/demo1/index.js
+++ plugins/demo1/index.js
@@ -5,7 +5,7 @@ import { preloadImages } from '../utils';
 // Preload  images
 preloadImages('.grid__item-img, .bigimg').then(() => {
     // Remove loader (loading class)
-    //document.body.classList.remove('loading');
+    document.body.classList.remove('loading');
     
     // Initialize grid
     const grid = new Grid(document.querySelector('.grid'));
