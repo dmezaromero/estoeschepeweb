8eaf4b99571d83fa2115998443ef36f01b90b87e
Dagoberto Meza <dagoberto.meza@spacelab.cr>
2022-12-20 12:19:55 -0600

    Update manifiesto.

--- pages/manifiesto.vue
+++ pages/manifiesto.vue
@@ -2,6 +2,7 @@
     <div class="uk-section">
         <div class="uk-container uk-container-expand">
             <h1>Manifiesto</h1>
+            <p>Esto es Chepe</p>
         </div>
     </div>
 </template>
